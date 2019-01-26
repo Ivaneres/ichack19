e8e3a0dac6b04d46f618027ec609e329466be819
Abel Shields <abel@uucp.org.uk>
2019-01-26 16:34:40 +0000

    Fixing GoogleSearch

--- LyricsMatch.py
+++ LyricsMatch.py
@@ -1,15 +1,23 @@
-import googlesearch
+maxResults = 50
+
+try:
+    #import googlesearch
+    from googlesearch.googlesearch import GoogleSearch
+    gSearchURLs = lambda search: [i.title for i in GoogleSearch().search(search, num_results=maxResults).results]
+except:
+    import googlesearch
+    gSearchURLs = lambda search: googlesearch.search(search, stop=maxResults)
+    
 import requests
 from bs4 import BeautifulSoup
-
-maxResults = 50
 
 ## megalobiz.com
 
 # @param lyrics - lyrics from user
 # @return - returns probable song name in tuple (artist, name)
 def get_song_from_lyrics(lyrics):
-    results = googlesearch.search(lyrics + " site:genius.com", stop=maxResults)
+    #results = googlesearch.search(lyrics + " site:genius.com", stop=maxResults)
+    results = gSearchURLs(lyrics + " site:genius.com")
     for result in results:
         if "GENIUS.COM" in result.upper():
             song_url = result
@@ -18,20 +26,13 @@ def get_song_from_lyrics(lyrics):
         print("Could not find song!")
         return None
 
-    
-
-    #print(lyrics_from_url(song_url))
     return details_from_genius_url(song_url)
-
-
 
 def details_from_genius_url(url):
   page = requests.get(url)
   html = BeautifulSoup(page.text, "html.parser")
-  #remove script tags that they put in the middle of the lyrics
   [h.extract() for h in html('script')]
-  #at least Genius is nice and has a tag called 'lyrics'!
-  lyrics = html.find("div", class_="lyrics").get_text()
+  #lyrics = html.find("div", class_="lyrics").get_text()
   pageTitle = html.find("title").get_text()
   print(pageTitle)
   print(pageTitle[:pageTitle.find(" Lyrics | Genius")])
@@ -39,3 +40,12 @@ def details_from_genius_url(url):
   artist, title = pageTitle[:pageTitle.find(" Lyrics | Genius")] \
                   .split(" – ")
   return artist, title
+
+
+def find_lyrics(url):
+  page = requests.get(url)
+  html = BeautifulSoup(page.text, "html.parser")
+  [h.extract() for h in html('script')]
+  lyrics = html.find("div", class_="tab-pane fade active in").get_text()
+  print(lyrics)
+  
