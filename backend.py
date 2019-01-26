852ae1f50d62fa092b2aed337ec4f029e48d0cdd
Max Kovalovs <maxkovalovs@gmail.com>
2019-01-26 15:37:20 +0000

    Added get_music endpoint

---
+++ backend.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+backend = Flask(__name__)
+
+
+@backend.route("/", methods=["GET"])
+def get_music(sung_text: str) -> str:
+    """
+    :param sung_text: lyrics that have been sung
+    :return: the URL to the music video/spotify
+    """
+    pass
