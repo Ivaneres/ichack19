8ff7a483a3e7ec94b438c156991590c646ae01ea
Ivan Ereshchenko <ereshchenkoi@gmail.com>
2019-01-26 22:51:50 +0000

    semi-working speech recog

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,18 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:speech_recognition/speech_recognition.dart';
 
 void main() => runApp(MyApp());
 
+SpeechRecognition _speech = new SpeechRecognition();
+
+  void activateSpeechRecognizer() {
+    print('_MyAppState.activateSpeechRecognizer... ');
+    _speech = new SpeechRecognition();
+    _speech.setAvailabilityHandler(onSpeechAvailability);
+    _speech.setCurrentLocaleHandler(onCurrentLocale);
+    _speech.setRecognitionStartedHandler(onRecognitionStarted);
+    _speech.setRecognitionResultHandler(onRecognitionResult);
+    _speech.setRecognitionCompleteHandler(onRecognitionComplete);
+    _speech
+        .activate()
+        .then((res) => setState(() => _speechRecognitionAvailable = res));
+  }
+
+void start() {
+  activateSpeechRecognizer();
+}
+
 class MyApp extends StatelessWidget {
+  SpeechRecognition _speech;
+
+  String transcript = "";
+
+  String lang = "en_US";
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'MadTunez',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
         body: Center(
           child: Text('Hello World'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: start,
         ),
       ),
     );
