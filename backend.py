4b55292c9ad26755b11a044d0c95e2b30489aa02
Abel <Abel@uucp.org.uk>
2019-01-27 04:21:29 +0000

    Fixing FUCKING HEROKU (v4)

--- backend.py
+++ backend.py
@@ -5,10 +5,12 @@ import os
 backend = Flask(__name__)
 
 @backend.route("/", methods=["POST"])
-def get_music():
+def get_music(a, b):
     """
     :return: the URL to the music video/spotify
     """
+    print(a)
+    print(b)
     song_name_artist_tuple = get_song_from_lyrics(request.json['lyrics'])
     json_dict =  {"songName": song_name_artist_tuple[0],
                   "artistName": song_name_artist_tuple[1]}
