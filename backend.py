529aa2ffbe595b261afe638e6fbb4399e163bf97
Abel <Abel@uucp.org.uk>
2019-01-27 05:33:20 +0000

    Server update

--- backend.py
+++ backend.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
-from LyricsMatch import get_song_from_lyrics
+from LyricsMatch import *
+from fuzzywuzzy import process
 import os
 
 backend = Flask(__name__)
@@ -9,21 +10,26 @@ def index():
     """
     :return: the URL to the music video/spotify
     """
-    print(dir(request))
-    print(str(request))
-    print(request.get_data())
-    print(request.get_json())
-    print(request.data)
-    print(request.json)
-        
-    js = request.get_json()
-    print(js)
     song_name_artist_tuple = get_song_from_lyrics(js['lyrics'])
-    json_dict =  {"songName": song_name_artist_tuple[0],
-                  "artistName": song_name_artist_tuple[1]}
-
+    
+    song = song_name_artist_tuple[0]
+    artist = song_name_artist_tuple[1]
+    
+    timestamps = find_lyrics_from_mbiz_url(find_mbiz_url(artist, song))
+    mostLikely = process.extractOne(lyrics, timestamps, processor=lambda x: x[1])
+    
+    # Will give us a tuple (timestamp, lyric)
+    # Want to return next timestamp, unless is last.
+    
+    mostLikelyTimestamp = timestamps[(timestamps.index(mostLikely)+1) % len(timestamps)]
+    
+    json_dict = {"songName"   : song,
+                 "artistName" : artist,
+                 "timestamp"  : mostLikelyTimeStamp }
+    
     return jsonify(json_dict)
 
+    
 if __name__ == "__main__":
     try:
         port = int(os.environ.get('PORT', 33507))
